9e88a435a6b032a7dc71100e323144be3eb363e2
Cristian Mfer <cmf@lcc.uma.es>
2022-07-21 02:19:48 +0200

    Information about first classses

---
+++ Apache_Kafka/consumer.py
@@ -0,0 +1,33 @@
+from confluent_kafka import Consumer
+
+def main():
+    
+    # Consumer creation
+    consumer = Consumer({
+        'bootstrap.servers': 'localhost:9094', # Kafka Brokers, configure properly
+        'group.id': 'incheon', # Group ID. Keep same ID for costumer in the group
+        'auto.offset.reset': 'earliest' # Offset configuration: earliest, latest
+    })
+
+    consumer.subscribe(['iot']) # Consumer subscribes to a list of topics
+
+    running = True
+    # Infinite loop until Ctrl->C is entered
+    while running:
+        try:
+            msg = consumer.poll(timeout=10) # Waits for messages 10s
+            if msg is None: # No message
+                continue
+            if msg.error(): # Error Message
+                print("Consumer error: {}".format(msg.error()))
+                continue
+            print("Message received from partition %d: %s value " % (msg.partition(),
+                                                                     str(msg.value().decode('utf8'))))
+        except KeyboardInterrupt:
+            running = False
+        
+    print("Bye!")
+    consumer.close()
+ 
+if __name__ == "__main__":
+    main()
---
+++ Apache_Kafka/producer.py
@@ -0,0 +1,29 @@
+from confluent_kafka import Producer
+import socket
+
+'''
+    Kafka Producer used in class
+'''
+
+__author__ = 'Cristian Martin'
+
+
+# Callback for message delivery confirmation
+def delivery_callback(err, msg):
+    if err is not None:
+        print("Failed to deliver message: %s: %s" % (str(msg), str(err)))
+    else:
+        print("Message sent to topic %s partition %s with latency %f" % (str(msg.topic()), 
+                                                                         str(msg.partition()), 
+                                                                         msg.latency()))
+
+def main():
+    conf = {'bootstrap.servers': "localhost:9094"} # Kafka Brokers, configure properly
+    producer = Producer(conf) # Define the producer
+    # Send a message with Kafka
+    producer.produce("iot", key="clave", value="Hello world!", callback=delivery_callback)
+    producer.poll(1) # Polls the producer for events and calls the corresponding callbacks 
+    producer.flush() # Wait for all messages in the Producer queue to be delivered
+    
+if __name__ == "__main__":
+    main()
